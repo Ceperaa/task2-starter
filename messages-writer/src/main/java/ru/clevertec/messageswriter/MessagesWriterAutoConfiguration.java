45b3475705a76c1d18e70c2446bbf8207790725f
Сергей <cepperaa@gmail.com>
2023-04-16 23:57:55 +0300

    @ConditionalOnMyCustom

--- messages-writer/src/main/java/ru/clevertec/messageswriter/MessagesWriterAutoConfiguration.java
+++ messages-writer/src/main/java/ru/clevertec/messageswriter/MessagesWriterAutoConfiguration.java
@@ -3,11 +3,13 @@ package ru.clevertec.messageswriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import ru.clevertec.messageswriter.conditional.ConditionalOnMyCustom;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 @ConditionalOnProperty(prefix = "print.message", name = "enabled", havingValue = "true")
+@ConditionalOnMyCustom
 @Slf4j
 public class MessagesWriterAutoConfiguration {
 
---
+++ messages-writer/src/main/java/ru/clevertec/messageswriter/conditional/ConditionalOnMyCustom.java
@@ -0,0 +1,14 @@
+package ru.clevertec.messageswriter.conditional;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Conditional(CustomCondition.class)
+public @interface ConditionalOnMyCustom {
+}
